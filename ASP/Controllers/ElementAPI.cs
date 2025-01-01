16ba24dfd3de558149b358a5f4e69a8438d601e2
Jechto <jetof20@student.sdu.dk>
2025-01-01 21:21:35 +0100

    begin merge api

---
+++ ASP/Controllers/ElementAPI.cs
@@ -0,0 +1,21 @@
+﻿using ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.Controllers
+{
+    public class ElementAPI : Controller
+    {
+        [HttpGet("/merge")]
+        public IActionResult MergeNames([FromQuery] string element1, [FromQuery] string element2)
+        {
+            if (string.IsNullOrEmpty(element1) || string.IsNullOrEmpty(element2))
+            {
+                return BadRequest("Both elements must be provided");
+            }
+
+            var mergedNames = $"{element1} {element2}";
+            return Json(mergedNames);
+        }
+    }
+}
--- ASP/Models/ErrorViewModel.cs
+++ ASP/Models/ErrorViewModel.cs
@@ -1,3 +1,5 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
 namespace ASP.Models
 {
     public class ErrorViewModel
@@ -6,4 +8,47 @@ namespace ASP.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
-}
+
+
+public static class ErrorViewModelEndpoints
+{
+	public static void MapErrorViewModelEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/ErrorViewModel").WithTags(nameof(ErrorViewModel));
+
+        group.MapGet("/", () =>
+        {
+            return new [] { new ErrorViewModel() };
+        })
+        .WithName("GetAllErrorViewModels")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            //return new ErrorViewModel { ID = id };
+        })
+        .WithName("GetErrorViewModelById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", (int id, ErrorViewModel input) =>
+        {
+            return TypedResults.NoContent();
+        })
+        .WithName("UpdateErrorViewModel")
+        .WithOpenApi();
+
+        group.MapPost("/", (ErrorViewModel model) =>
+        {
+            //return TypedResults.Created($"/api/ErrorViewModels/{model.ID}", model);
+        })
+        .WithName("CreateErrorViewModel")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            //return TypedResults.Ok(new ErrorViewModel { ID = id });
+        })
+        .WithName("DeleteErrorViewModel")
+        .WithOpenApi();
+    }
+}}
